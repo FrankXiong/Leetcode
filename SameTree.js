d14a5f3a8429a9def22d83a4ce888eaa88fe4aa6
FrankXiong <1131076966@qq.com>
2017-01-13 22:32:48 +0800

    simplify code

--- SameTree.js
+++ SameTree.js
@@ -12,40 +12,36 @@ Two binary trees are considered equal if they are structurally identical and the
  * }
  */
 
- // Solution A: Recursive
+// Solution A: Recursive
 /**
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if(p === null && q === null) {
-        return true;
-    }
-    if(p === null || q === null) {
-        return false;
-    }
-    if(p.val !== q.val) {
-        return false;
-    } else {
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    }
+	if (!p && !q) return true;
+	if (!p || !q) return false;
+	if (p.val !== q.val) {
+		return false;
+	} else {
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
 };
 
 // Solution B: Non-recursive
 var isSameTree = function(p, q) {
-    let stack = [{p, q}];
-    while(stack.length > 0) {
-        let o = stack.pop();
-        let a = o.p, b = o.q;
-        if(!a && !b) continue;
-        if(!a || !b) return false;
-        if(a.val !== b.val) {
-            return false;
-        } else {
-            stack.push({p:a.left, q:b.left});
-            stack.push({p:a.right, q:b.right});
-        }
-    }
-    return true;
+	let stack = [{ p, q }];
+	while (stack.length > 0) {
+		let o = stack.pop();
+		let a = o.p, b = o.q;
+		if (!a && !b) continue;
+		if (!a || !b) return false;
+		if (a.val !== b.val) {
+			return false;
+		} else {
+			stack.push({ p: a.left, q: b.left });
+			stack.push({ p: a.right, q: b.right });
+		}
+	}
+	return true;
 };
